ee50929a27b7d1297fd4504f086fa5370ad7e3c3
moriarty <sherlock0909@icloud.com>
2020-10-23 09:16:15 +0800

    优化redis配置类

--- cloud-admin/src/main/java/com/ccssoft/cloudadmin/config/RedisConfig.java
+++ cloud-admin/src/main/java/com/ccssoft/cloudadmin/config/RedisConfig.java
@@ -23,28 +23,47 @@ public class RedisConfig {
     @SuppressWarnings("all")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
         RedisTemplate<String, Object> template = new RedisTemplate();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        //Json序列化配置
-        Jackson2JsonRedisSerializer<Object> objectJackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        objectJackson2JsonRedisSerializer.setObjectMapper(om);
-        //String的序列化配置
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        //key采用String的序列化方式
-        template.setKeySerializer(stringRedisSerializer);
-        //hash的key也采用String的序列化方式
-        template.setHashKeySerializer(stringRedisSerializer);
-        //value序列化方式采用jackson
-        template.setValueSerializer(objectJackson2JsonRedisSerializer);
-        //hash的value序列化方式采用jackson
-        template.setHashValueSerializer(objectJackson2JsonRedisSerializer);
-
-        template.afterPropertiesSet();
-
+        setSerializer(template);
         return template;
+//        RedisTemplate<String, Object> template = new RedisTemplate();
+//        template.setConnectionFactory(redisConnectionFactory);
+//
+//        //Json序列化配置
+//        Jackson2JsonRedisSerializer<Object> objectJackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+//        ObjectMapper om = new ObjectMapper();
+//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        objectJackson2JsonRedisSerializer.setObjectMapper(om);
+//        //String的序列化配置
+//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//        //key采用String的序列化方式
+//        template.setKeySerializer(stringRedisSerializer);
+//        //hash的key也采用String的序列化方式
+//        template.setHashKeySerializer(stringRedisSerializer);
+//        //value序列化方式采用jackson
+//        template.setValueSerializer(objectJackson2JsonRedisSerializer);
+//        //hash的value序列化方式采用jackson
+//        template.setHashValueSerializer(objectJackson2JsonRedisSerializer);
+//
+//        template.afterPropertiesSet();
+//
+//        return template;
+    }
 
+    private void setSerializer(RedisTemplate<String, Object> redisTemplate) {
+        //设置键（key）的序列化方式
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        // 全局开启AutoType，不建议使用
+        // ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        // 建议使用这种方式，小范围指定白名单
+//        ParserConfig.getGlobalInstance().addAccept("com.xxx.");
+
+        // 设置键（key）的序列化采用StringRedisSerializer。
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        // 设置值（value）的序列化采用FastJsonRedisSerializer。
+        redisTemplate.setValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
     }
 }
